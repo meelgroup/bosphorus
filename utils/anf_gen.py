cbefb0cad2a85635290ff3f7a984755f9cce6e00
Mate Soos <soos.mate@gmail.com>
2019-05-10 21:20:09 +0200

    Adding ANF generator

---
+++ utils/anf_gen.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2014  Mate Soos
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 2
+# of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+
+
+from __future__ import with_statement  # Required in 2.5
+from __future__ import print_function
+import sys
+import random
+
+def get_monomial():
+    deg = int(random.gammavariate(0.2, 4))+1
+    assert deg >0
+
+    already_in = []
+    ret = ""
+    for x in range(deg):
+        if x != 0:
+            ret += "*"
+        ret += "x(%d)" % random.randint(0, max_var)
+
+    return ret
+
+
+def gen_equation():
+    num_monoms = int(random.gammavariate(0.9, 20))+1
+    assert num_monoms >0
+
+    ret = ""
+    for x in range(num_monoms):
+        if x != 0:
+            ret += "+"
+
+        ret += get_monomial()
+
+    return ret
+
+
+if len(sys.argv) > 2:
+    print("Usage: anf_gen.py [seed]")
+
+
+seed = random.randint(0, 1000000)
+if len(sys.argv) == 2:
+    try:
+        seed = sys.argv[1]
+    except ValueError:
+        print("ERROR: seed must be an integer")
+        exit(-1)
+
+
+random.seed(seed)
+max_var = random.randint(10, 100)
+num_eqs = random.randint(1, 100)
+for x in range(num_eqs):
+    eq = gen_equation()
+    print(eq)
+
