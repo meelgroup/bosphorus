6a18b263a569e8a4c06af8598eba269eac711517
Mate Soos <soos.mate@gmail.com>
2020-03-03 21:43:32 +0100

    Moving on with solution parser

--- utils/map_solution.py
+++ utils/map_solution.py
@@ -82,120 +82,129 @@ def parse_solution(fname):
 
     return unsat, solution
 
-if len(sys.argv) != 3:
-    print("ERROR: You must give 2 arguments: map file and solution file")
-    print("Usage: map_solution.py map_file solution_file")
-    exit(-1)
 
-
-mapfile = sys.argv[1]
-solfile = sys.argv[2]
-
-print("c solution file: ", solfile)
-print("c map file: ", mapfile)
-
-# parse up solution
-unsat, cnfsol = parse_solution(solfile)
-if unsat:
-    print("s UNSATISFIABLE")
-    exit(0)
-# print ("CNF solution: ", cnfsol)
-
-
-anfsol = {}
-with open(mapfile, "r") as f:
-    for line in f:
-        line = line.strip()
-        line = line.split()
-        if len(line) == 0:
-            continue
-
-        if line[0] == "Internal-ANF-var":
-            assert len(line) == 5, "Map file corrupted!"
-            try:
-                var = int(line[4])
-            except ValueError:
-                print("ERROR: Map file is corrupt, cannot parse variable: ", line[4])
-                print("Tried parsing line:", line)
-                exit(-1)
-
-            assert var >= 0
-            if var not in cnfsol:
-                anfsol[var] = None
-            else:
-                anfsol[var] = cnfsol[var]
-
-        elif line[0] == "ANF-var-val":
-            try:
-                var = int(line[1])
-            except ValueError:
-                print("ERROR: Map file is corrupt, cannot parse variable: ", line[3])
-                print("Tried parsing line:", line)
-                exit(-1)
-
-            val = None
-            if line[3] == "l_True":
-                val = True
-            elif line[3] == "l_False":
-                val = False
-            else:
-                print("ERROR: Map file is corrupt, cannot parse: ", line[3])
-                print("Tried parsing line:", line)
-                exit(-1)
-
-            anfsol[var] = val
-
-        elif line[0] == "must-set-ANF-var-to-any":
-            try:
-                var = int(line[1])
-            except ValueError:
-                print("ERROR: Map file is corrupt, cannot parse variable: ", line[1])
-                print("Tried parsing line:", line)
-                exit(-1)
-
-            # any value works, so setting to TRUE
-            anfsol[var] = True
-
-        elif line[0] == "ANF-var":
-            try:
-                var1 = int(line[1])
-                var2 = int(line[4])
-                inv = int(line[6])
-            except ValueError:
-                print("ERROR: Map file is corrupt")
-                print("Tried parsing line:", line)
-                exit(-1)
-
-            assert line[5] == "^", "Map file is corrupt"
-            assert var1 >= 0, "Map file is corrupt"
-            assert var2 >= 0, "Map file is corrupt"
-            assert inv == 0 or inv == 1, "Map file is corrupt"
-
-            val = int(anfsol[var2]) ^ inv
-            if val == 0:
-                realval = False
-            elif val == 1:
-                realval = True
-            else:
-                assert False
-
-            anfsol[var1] = realval
-
-        else:
-            print("Map file corrupt, cannot understand line: ", line)
-
-# print("ANF solution:", anfsol)
-sol_in_txt = ""
-for a,b in anfsol.items():
-    if b is None:
+def parse_one_map_line(line, anfsol):
+    line = line.strip()
+    line = line.split()
+    if len(line) == 0:
         continue
 
-    extra = ""
-    if not b:
-        extra = "-"
-    sol_in_txt += "%s%d " %(extra, a)
+    if line[0] == "Internal-ANF-var":
+        assert len(line) == 5, "Map file corrupted!"
+        try:
+            var = int(line[4])
+        except ValueError:
+            print("ERROR: Map file is corrupt, cannot parse variable: ", line[4])
+            print("Tried parsing line:", line)
+            exit(-1)
 
-print("c solution below, with variables starting at 0, as per ANF convention.")
-print("c preceding '-' means it's False, so '-0' has a meaning")
-print("s ANF-SATISFIABLE")
-print("v %s" % sol_in_txt)
+        assert var >= 0
+        if var not in cnfsol:
+            anfsol[var] = None
+        else:
+            anfsol[var] = cnfsol[var]
+
+    elif line[0] == "ANF-var-val":
+        try:
+            var = int(line[1])
+        except ValueError:
+            print("ERROR: Map file is corrupt, cannot parse variable: ", line[3])
+            print("Tried parsing line:", line)
+            exit(-1)
+
+        val = None
+        if line[3] == "l_True":
+            val = True
+        elif line[3] == "l_False":
+            val = False
+        else:
+            print("ERROR: Map file is corrupt, cannot parse: ", line[3])
+            print("Tried parsing line:", line)
+            exit(-1)
+
+        anfsol[var] = val
+
+    elif line[0] == "must-set-ANF-var-to-any":
+        try:
+            var = int(line[1])
+        except ValueError:
+            print("ERROR: Map file is corrupt, cannot parse variable: ", line[1])
+            print("Tried parsing line:", line)
+            exit(-1)
+
+        # any value works, so setting to TRUE
+        anfsol[var] = True
+
+    elif line[0] == "ANF-var":
+        try:
+            var1 = int(line[1])
+            var2 = int(line[4])
+            inv = int(line[6])
+        except ValueError:
+            print("ERROR: Map file is corrupt")
+            print("Tried parsing line:", line)
+            exit(-1)
+
+        assert line[5] == "^", "Map file is corrupt"
+        assert var1 >= 0, "Map file is corrupt"
+        assert var2 >= 0, "Map file is corrupt"
+        assert inv == 0 or inv == 1, "Map file is corrupt"
+
+        val = int(anfsol[var2]) ^ inv
+        if val == 0:
+            realval = False
+        elif val == 1:
+            realval = True
+        else:
+            assert False
+
+        anfsol[var1] = realval
+
+    else:
+        print("Map file corrupt, cannot understand line: ", line)
+
+def parse_anf_sol(mapfile):
+    anfsol = {}
+    with open(mapfile, "r") as f:
+        for line in f:
+            parse_one_map_line(line, anfsol)
+
+    return anfsol
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 3:
+        print("ERROR: You must give 2 arguments: map file and solution file")
+        print("Usage: map_solution.py map_file solution_file")
+        exit(-1)
+
+
+    mapfile = sys.argv[1]
+    solfile = sys.argv[2]
+    print("c solution file: ", solfile)
+    print("c map file: ", mapfile)
+
+    # parse up solution
+    unsat, cnfsol = parse_solution(solfile)
+    if unsat:
+        print("s UNSATISFIABLE")
+        exit(0)
+    # print ("CNF solution: ", cnfsol)
+
+    anfsol = parse_anf_sol(mapfile)
+    # print("ANF solution:", anfsol)
+
+    sol_in_txt = ""
+    for a,b in anfsol.items():
+        if b is None:
+            continue
+
+        extra = ""
+        if not b:
+            extra = "-"
+        sol_in_txt += "%s%d " %(extra, a)
+
+    print("c solution below, with variables starting at 0, as per ANF convention.")
+    print("c preceding '-' means it's False, so '-0' has a meaning")
+    print("s ANF-SATISFIABLE")
+    print("v %s" % sol_in_txt)
