e7b2969ffeb332d238c98be39d14b2e356caac80
Mate Soos <soos.mate@gmail.com>
2023-02-05 19:09:18 +0100

    Always export CMAKE commands

--- utils/build_norm.sh
+++ utils/build_norm.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 rm -rf CMake* cm* lib* bosph* Testing* tests* include tests* CPack*
-cmake -DENABLE_TESTING=ON ..
+cmake -DENABLE_TESTING=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 make -j24
