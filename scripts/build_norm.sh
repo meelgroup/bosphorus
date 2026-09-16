00c20e852100bb1ad70071799e487860a8e87dca
Mate Soos <soos.mate@gmail.com>
2026-09-17 00:18:05 +0200

    Fixing build_norm

--- scripts/build_norm.sh
+++ scripts/build_norm.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/bash
 
 rm -rf CMake* cm* lib* bosph* Testing* tests* include tests* CPack*
-cmake -DENABLE_TESTING=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
-make -j24
+cmake -DENABLE_TESTING=ON \
+    -Dcadical_DIR=../../cadical/build \
+    -Dcadiback_DIR=../../cadiback/build \
+    -Dcryptominisat5_DIR=../../cryptominisat/build \
+    ..
+make -j
 make test
